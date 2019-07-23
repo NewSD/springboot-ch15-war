3ca7b84c99fe8afa0ae3ce29c1f72a7f1e60bad0
18300603955 <oceaniu@163.com>
2019-07-23 15:03:37 +0800

    war init

---
+++ src/main/java/com/niu/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.niu;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringbootCh15WarApplication.class);
+	}
+
+}
---
+++ src/main/java/com/niu/SpringbootCh15WarApplication.java
@@ -0,0 +1,13 @@
+package com.niu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCh15WarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCh15WarApplication.class, args);
+	}
+
+}
